2743f523104b8672107d0bead3e211fad4384ed4
Asterios Raptis <asteri.raptis@gmail.com>
2026-06-02 18:07:43 +0200

    chore(release): bump version to v1.53.1

--- install.ps1
+++ install.ps1
@@ -15,7 +15,7 @@
 
 $ErrorActionPreference = "Stop"
 
-$Version = if ($env:ADAPTIVE_LEARNER_VERSION) { $env:ADAPTIVE_LEARNER_VERSION } else { "v1.53.0" }
+$Version = if ($env:ADAPTIVE_LEARNER_VERSION) { $env:ADAPTIVE_LEARNER_VERSION } else { "v1.53.1" }
 $Repo = "astrapi69/adaptive-learner"
 $InstallDir = if ($env:ADAPTIVE_LEARNER_DIR) { $env:ADAPTIVE_LEARNER_DIR } else { Join-Path $HOME "adaptive-learner" }
 
